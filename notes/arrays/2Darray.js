bed13336421dc1b1a57110688ca1206ad91e2b5a
Nandhakishor <nandhakishork0@gmail.com>
2026-08-11 13:41:34 +0530

    changed

---
+++ notes/arrays/2Darray.js
@@ -0,0 +1,39 @@
+// 2Darray
+// let arr = [
+//     [10, 20, 30],
+//     [40, 50, 60],
+//     [70, 80, 90]
+// ];
+// for(let i=0;i<arr.length;i++){
+
+//     for(let j=0;j<arr.length;j++){
+
+//             console.log(arr[i][j])
+//     }
+// }
+// for(let i=0;i<arr.length;i++){
+//     let row= "";
+//     for(let j=0;j<arr.length;j++){
+//         row+= arr[i][j]+ " "
+//     }
+//     console.log(row)
+// }
+// let sum=0
+// for(let i=0;i<arr.length;i++){
+//     for(let j=0;j<arr.length;j++){
+//         sum=sum+arr[i][j]
+//     }
+// }
+// console.log(sum)
+
+let arr = [
+    [10, 20, 30],
+    [40, 50, 60],
+    [70, 80, 90]
+];
+
+for(let i=0;i<arr.length;i++){
+    for(let j=0;j<arr.length;j++){
+        console.log(arr[0][2])
+    }
+}
