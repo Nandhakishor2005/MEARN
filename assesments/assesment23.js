da600e72f130cfe108ea247cfd5eb7a391d96aff
Nandhakishor <nandhakishork0@gmail.com>
2026-08-13 21:42:57 +0530

    changed

---
+++ assesments/assesment23.js
@@ -0,0 +1,26 @@
+// let arr=[1,2,3,4,5];
+// let target =20;
+// let sum=0;
+// for(let i=0;i<arr.length;i++){
+//     sum=sum+arr[i]
+// }
+// console.log(sum+target)
+
+// let arr=[1,25,3,45,50];
+// let value =20;
+// let temp=[];
+// count=0;
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]>20){
+//         temp[count]=arr[i]
+//         count++;
+//     }
+// }
+// console.log(temp)
+
+//console.log(typeof(NaN))
+
+// let arr=[];
+// arr.pop();
+// console.log(arr)
+
