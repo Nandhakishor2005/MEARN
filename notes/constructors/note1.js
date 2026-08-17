80a1686e4c30982b972efd0fca0136e64dde3d44
Nandhakishor <nandhakishork0@gmail.com>
2026-08-17 16:40:42 +0530

    updated

--- notes/constructors/note1.js
+++ notes/constructors/note1.js
@@ -1,15 +1,19 @@
-function Student(id, name, age, course){
+function Student(id, name, age, course){  // constructor
     this.id = id;
     this.name = name;
     this.age = age;
     this.course = course;
+
 
 }
 Student.prototype.display =function(){
     console.log("hii")
 }
 
-const Student1= new Student(1, "arun",20, "mearn");
+const Student1= new Student(1, "arun",20, "mearn"); // object
 const Student2= new Student(2,"rahul",22,"python");
 
 console.log(Student2)
+console.log(Student.prototype);
+console.log(Student1.__proto__)
+Student1.display();
