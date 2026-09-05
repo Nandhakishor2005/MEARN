d08ef0d69ec741a2c381832a5d8bb251e604146b
Nandhakishor <nandhakishork0@gmail.com>
2026-09-05 10:24:00 +0530

    updated

--- notes/OOPs/Class/math.js
+++ notes/OOPs/Class/math.js
@@ -1,15 +1,21 @@
-function add(a,b){
+// used for named export
+
+// export function add(a,b){
+//     return a+b
+// }
+
+// export function sub(a,b){
+//     return a-b;
+// }
+
+// export function mul(a,b){
+//     return a*b;
+// }
+
+// export function div(a,b){
+//     return a/b;
+// }
+
+export default function add(a,b){
     return a+b
-}
-
-function sub(a,b){
-    return a-b;
-}
-
-function mul(a,b){
-    return a*b;
-}
-
-function div(a,b){
-    return a/b;
 }
---
+++ notes/OOPs/Class/new.js
@@ -0,0 +1,12 @@
+// const math = require("./math");
+// console.log(math.add(10,20))
+// console.log(math.mul(10,10))
+
+
+
+// named export
+import add from "./math.js"
+console.log(add(10,20))
+// console.log(sub(100,20))
+// console.log(mul(10,20))
+// console.log(div(100,20))
