8ce41ffd79ed20697787ecff4b22923a426294f3
Nandhakishor <nandhakishork0@gmail.com>
2026-09-08 11:14:30 +0530

    updated

---
+++ notes/OOPs/recursion/r1.js
@@ -0,0 +1,30 @@
+// function count(n){
+//     if(n==0){
+//         console.log("count stopped");
+//         return ;
+//     }
+//     console.log(n);
+//     count(n-1);
+// }
+// count(5);
+
+// function fact(f){
+
+//     if(f==1){
+//         return f;
+//     }
+//     return f*fact(f-1);
+
+// }
+
+// console.log(fact(5));
+
+function count(n){
+    if(n==0){
+        return  ;
+    }
+    count(n-1);
+    console.log(n);
+}
+count(5)
+console.log("count stopped");
