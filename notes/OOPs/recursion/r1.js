541839d7d26b762f942401f9faf5ba492bd6ad9a
Nandhakishor <nandhakishork0@gmail.com>
2026-09-09 10:11:00 +0530

    recursion

--- notes/OOPs/recursion/r1.js
+++ notes/OOPs/recursion/r1.js
@@ -29,12 +29,14 @@
 // count(5)
 // console.log("count stopped");
 
+// sum of digits
+
 function sumOfDigits(n){
     if(n==0){
-        return;
+        return 0;
     }
-    
+    return n%10 + sumOfDigits(Math.floor(n/10))
 }
-console.log(sumOfDigits(5))
+console.log(sumOfDigits(123))
 
 // sum of numbers
