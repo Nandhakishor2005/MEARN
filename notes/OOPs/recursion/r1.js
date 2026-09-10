ff9c0b62af4363584edf9eda0b5657120339fa08
Nandhakishor <nandhakishork0@gmail.com>
2026-09-10 09:16:50 +0530

    edited

--- notes/OOPs/recursion/r1.js
+++ notes/OOPs/recursion/r1.js
@@ -31,12 +31,12 @@
 
 // sum of digits
 
-function sumOfDigits(n){
-    if(n==0){
-        return 0;
-    }
-    return n%10 + sumOfDigits(Math.floor(n/10))
-}
-console.log(sumOfDigits(123))
+// function sumOfDigits(n){
+//     if(n==0){
+//         return 0;
+//     }
+//     return n%10 + sumOfDigits(Math.floor(n/10))
+// }
+// console.log(sumOfDigits(123))
 
-// sum of numbers
+
